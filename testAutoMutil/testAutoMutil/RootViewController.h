52d28f65ac95a4b293acf76cd5e483949cd74e07
nicky <huangznian@163.com>
2017-03-17 17:41:44 +0800

    new Pro

---
+++ testAutoMutil/testAutoMutil/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  testAutoMutil
+//
+//  Created by nicky on 2017/2/24.
+//  Copyright © 2017年 nicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ testAutoMutil/testAutoMutil/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testAutoMutil
+//
+//  Created by nicky on 2017/2/24.
+//  Copyright © 2017年 nicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
